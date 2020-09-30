6d1fc3144e590aa9a74dc6b17f6662bc0b116e2d
krusherks <38016912+krusherks@users.noreply.github.com>
2020-10-01 01:51:43 +0530

    update reduce size

--- static/js/script.js
+++ static/js/script.js
@@ -9,31 +9,7 @@ $(document).ready(function(){
 			$('div.carouselNext, div.carouselPrev').removeClass('visible');
 	});	
 	
-	// Flip card to the back side
-	$('div.view_details').click(function(){		
-		$('div.carouselNext, div.carouselPrev').removeClass('visible');
-		$('div.product-card').addClass('flip-10');
-		setTimeout(function(){
-			$('div.product-card').removeClass('flip-10').addClass('flip90').find('div.shadow').show().fadeTo( 80 , 1, function(){
-				$('div.product-front, div.product-front div.shadow').hide();			
-			});
-		}, 50);
-		
-		setTimeout(function(){
-			$('div.product-card').removeClass('flip90').addClass('flip190');
-			$('div.product-back').show().find('div.shadow').show().fadeTo( 90 , 0);
-			setTimeout(function(){				
-				$('div.product-card').removeClass('flip190').addClass('flip180').find('div.shadow').hide();						
-				setTimeout(function(){
-					$('div.product-card').css('transition', '100ms ease-out');			
-					$('div.cx, div.cy').addClass('s1');
-					setTimeout(function(){$('div.cx, div.cy').addClass('s2');}, 100);
-					setTimeout(function(){$('div.cx, div.cy').addClass('s3');}, 200);				
-					$('div.carouselNext, div.carouselPrev').addClass('visible');				
-				}, 100);
-			}, 100);			
-		}, 150);			
-	});			
+	
 	
 	// Flip card back to the front side
 	$('div.flip-back').click(function(){		
